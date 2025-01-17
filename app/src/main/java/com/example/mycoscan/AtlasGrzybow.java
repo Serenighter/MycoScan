ed8bb44d2fa59ee701c5c5c7a3d1d1ca5f8e8993
Krystian <krystian.klacza@student.wsb.edu.pl>
2025-01-17 20:56:23 +0100

    Add database (SQLite) integration

--- app/src/main/java/com/example/mycoscan/AtlasGrzybow.java
+++ app/src/main/java/com/example/mycoscan/AtlasGrzybow.java
@@ -1,19 +1,64 @@
 package com.example.mycoscan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-public class AtlasGrzybow extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class AtlasGrzybow extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atlas_grzybow);
 
-        if (savedInstanceState == null) {
-            DropdownMenuFragment dropdownFragment = DropdownMenuFragment.newInstance("Atlas Grzybów");
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, dropdownFragment)
-                    .commit();
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+
+    public void kot(View view) {
+        Intent intent = new Intent(this, Pomoc.class);
+        startActivity(intent);
+    }
+
 }
